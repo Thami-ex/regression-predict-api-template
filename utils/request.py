8adeec8c9738045f437e2bb6ddde04c2c94574e0
Prescam3 <60913826+Prescam3@users.noreply.github.com>
2020-10-20 10:52:18 +0200

    the correct remote ip adrress

--- utils/request.py
+++ utils/request.py
@@ -39,7 +39,7 @@ feature_vector_json = test.iloc[1].to_json()
 
 # url = 'http://{public-ip-address-of-remote-machine}:5000/api_v0.1'
 #url = 'http://127.0.0.1:5000/api_v0.1' 
-url = 'http://52.215.65.127:5000/api_v0.1'
+url = 'http://54.229.92.198:5000/api_v0.1'
 
 
 
